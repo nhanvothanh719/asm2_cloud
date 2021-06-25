b8f2d7eaec287f9bbaee4a0c04e03e77af4f35df
nhanvtgcd191366 <84842522+nhanvtgcd191366@users.noreply.github.com>
2021-06-25 15:27:10 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,7 @@
 <?php
 	#DATABASE CREDENTIAL
 	$db_host = "ec2-54-197-100-79.compute-1.amazonaws.com";
-	//$db_name = "d1om80ajbvaek4";
-	$db_name = "no name";
+	$db_name = "d1om80ajbvaek4";
 	$db_user = "vxoeeolthvyckd";
 	$db_password = "f0e1ea719887ccd1b879dc020cee0d76c8de794a579d7aa43e218195ba4c4106";
 	$conn_string = "host = $db_host port = 5432 dbname = $db_name user = $db_user password = $db_password";
